61fcb1fdbab440fff3f751f3575a34fb3e6b90a2
SherryLiHj <72535835+LessIsMo@users.noreply.github.com>
2022-09-28 11:05:48 +0800

    Update Solution.java

--- javacoder/Solution.java
+++ javacoder/Solution.java
@@ -25,6 +25,58 @@ public class Solution {
         }
         return null;
     }
+    
+    // 2. three sum（hash / double pointer）
+    // hash
+    public List<List<Integer>> threeSum(int[] nums){
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Arrays.sort(nums); // 先给数组排序，方便之后查重
+        for(int i=0; i<nums.length-2; i++){
+            Map<Integer,Integer> map = new HashMap<>();
+            for(int j=i+1; j<nums.length; j++){
+                int temp = 0 - nums[i] - nums[j];
+                if(map.containsKey(temp)){
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(temp);
+                    list.add(nums[j]);
+                    if(!result.contains(list))
+                        result.add(list);
+                }
+                map.put(nums[j],j);
+            }
+        }
+        return result;
+    }
+    
+    // double pointer
+    public List<List<Integer>> threeSum_double(int[] nums){
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        int len = nums.length;
+        Arrays.sort(nums);
+        for (int i=0; i<len-2; i++){
+            if(nums[i] > 0)
+                return result;
+            if(i>0 && nums[i] == nums[i-1])
+                continue;
+            for (int j=i+1; j<len-1; j++){
+                if(j>i+1 && nums[j] == nums[j-1])
+                    continue;
+                for (int k = len-1; k>j; k--) {
+                    if(nums[i] + nums[j] + nums[k] < 0)
+                        break;
+                    if ((k == len - 1 || nums[k] != nums[k + 1]) && nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        result.add(list);
+                    }
+                }
+            }
+        }
+        return result;
+    }
 
     public static void main(String[] args){
         Two_Sum ts = new Two_Sum();
