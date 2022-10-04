5deaa932c48f49d8e4c2df7bb14b824cf390f8e6
SherryLiHj <72535835+SherryLiHj@users.noreply.github.com>
2022-10-04 13:36:24 +0800

    Update Solution.java

--- javacoder/Solution.java
+++ javacoder/Solution.java
@@ -113,5 +113,43 @@ public class Solution {
         Two_Sum ts = new Two_Sum();
         System.out.println(ts.twoSum(new int[]{2,7,11,5}, 9));
     }
+    
+    // 20. Merge Two Sorted Lists( Iteration / Recursion)
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) { // Iteration
+        ListNode prehead = new ListNode(0,null);
+        ListNode result = prehead;
+        while(list1!=null && list2!=null){
+            if(list1.val < list2.val){
+                result.next = list1;
+                list1 = list1.next;
+            } else {
+                result.next = list2;
+                list2 = list2.next;
+            }
+            result = result.next;
+        }
+        if(list1 != null)
+            result.next = list1;
+        else
+            result.next = list2;
+        return prehead.next;
+    }
+
+    public ListNode mergeTwoListsRecursion(ListNode list1, ListNode list2) { // Recursion 只关注这一层要干什么,返回什么,至于下一层(规模减1),不管
+        if(list1 == null)
+            return list2;
+        else if(list2 == null)
+            return list1;
+        else if(list1.val < list2.val) {
+            list1.next = mergeTwoListsRecursion(list1.next, list2);
+            return list1;
+        }
+        else {
+            list2.next = mergeTwoListsRecursion(list1, list2.next);
+            return list2;
+        }
+    }
+    
+    
 }
 
